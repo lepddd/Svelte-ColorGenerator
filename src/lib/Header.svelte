ce0194a351b84a0a2a247bcdcee2058324ac82ad
Luiz Dornelas <luizeduardojf@hotmail.com>
2022-12-16 19:23:45 -0300

    refactored code

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,99 +1,25 @@
 <script>
-  import Icon from "@iconify/svelte";
-  import ColorPicker from "./ColorPicker.svelte";
-  import NewColorBtn from "./NewColorBtn.svelte";
-  import HexInput from "./HexInput.svelte";
+  import NewBtn from "./NewBtn.svelte";
 
-  let title, show;
-
-  title = "Colors";
-
-  show = true;
-
-  /* Call function when resize window */
-  function ObserverWidth() {
-    if (innerWidth <= 500) {
-      show = false;
-    } else {
-      show = true;
-    }
-  }
+  let title = "Colors";
 </script>
-
-<svelte:window on:resize={ObserverWidth} />
 
 <header>
   <span class="title">{title}</span>
-  <button class="close-menu" on:click={() => (show = !show)}>
-    <Icon icon="ep:menu" color="#212529" width="24" height="24" />
-  </button>
-  {#if show}
-    <span class="right">
-      <div class="color-options">
-        <ColorPicker />
-        <HexInput />
-      </div>
-      <NewColorBtn on:click />
-    </span>
-  {/if}
+  <NewBtn />
 </header>
 
 <style>
   header {
     display: flex;
     align-items: center;
-    height: 60px;
     justify-content: space-between;
-    padding: 0 20px;
-  }
-  .close-menu {
-    display: none;
-    align-items: center;
-    background-color: #fff;
-    border-radius: 50%;
-    border: none;
-    cursor: pointer;
-    height: 40px;
-    justify-content: center;
-    width: 40px;
-  }
-  .close-menu:focus,
-  .close-menu:hover {
-    background-color: rgb(225, 225, 225);
-  }
-  .right {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    padding-left: 20px;
-  }
-  .color-options {
-    display: flex;
-    align-items: center;
+    padding: 20px;
+    min-width: 320px;
   }
   .title {
-    position: relative;
     color: #212529;
     font-size: 32px;
     font-weight: 700;
-  }
-  @media screen and (max-width: 500px) {
-    .close-menu {
-      display: flex;
-    }
-    .right {
-      position: absolute;
-      top: 60px;
-      left: 0;
-      width: 100%;
-      z-index: 99;
-      min-width: 320px;
-      background-color: #fff;
-      padding: 10px 20px;
-      border-radius: 5px;
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25),
-        0px 1px 3px 1px rgba(60, 64, 67, 0.15);
-    }
   }
 </style>
---
+++ src/lib/NewBtn.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Icon from "@iconify/svelte";
+  import { color } from "../colorStore";
+</script>
+
+<button on:click={color.newColor}>
+  <Icon icon="ic:round-shuffle" color="#212529" width="24" height="24" />
+</button>
+
+<style>
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f4f4f4;
+    border-radius: 50%;
+    border: 1px solid #f4f4f4;
+    cursor: pointer;
+    height: 50px;
+    width: 50px;
+    transition: 0.3s ease-in-out;
+  }
+  button:focus,
+  button:hover {
+    border: 1px solid #ededed;    
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.06), 0px 4px 3px rgba(0, 0, 0, 0.07);
+  }
+</style>
